68f4bab38ce418aa0c25ccc515a72335c972af1a
JGM01 <jacobgermanamccray@gmail.com>
2025-04-28 23:38:34 -0400

    search works! O(n) runtime. I also tried to functional-ize some of the code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,38 +5,51 @@
 	let { children } = $props();
     const isRootPath = $derived(page.url.pathname === '/');
 
-interface PostMetadata {
-    title: string;
-    summary: string;
-    date: string; 
-    tags: string[]; 
-  }
-
-  interface Post {
-    slug: string;
-    metadata: PostMetadata;
-  }
-
-  const postModules = import.meta.glob<{ metadata: PostMetadata }>(
-    '../posts/*.svx',
-    { eager: true }
-  );
-
-  const posts: Post[] = Object.keys(postModules).map((path) => {
-    const slug = path.split('/').pop()?.replace('.svx', '') ?? '';
-    return {
-      slug,
-      metadata: postModules[path].metadata || { title: 'Untitled' }
-    };
-  });
-
-  posts.sort((a, b) => {
-    if (a.metadata.date && b.metadata.date) {
-      return new Date(b.metadata.date).getTime() - new Date(a.metadata.date).getTime();
+    interface PostMetadata {
+        title: string;
+        summary: string;
+        date: string; 
+        tags: string[]; 
     }
-    return a.metadata.title.localeCompare(b.metadata.title);
-  });
-    console.log('Loaded posts:', posts);
+
+    interface Post {
+        slug: string;
+        metadata: PostMetadata;
+    }
+
+    const postModules = import.meta.glob<{ metadata: PostMetadata }>(
+        '../posts/*.svx',
+        { eager: true }
+    );
+
+    const posts: Post[] = Object.keys(postModules).map((path) => {
+        const slug = path.split('/').pop()?.replace('.svx', '') ?? '';
+        return {
+            slug,
+            metadata: postModules[path].metadata || { title: 'Untitled' }
+        };
+    });
+
+    const sortedPosts = [...posts].sort((a, b) => {
+        if (a.metadata.date && b.metadata.date) {
+            return new Date(b.metadata.date).getTime() - new Date(a.metadata.date).getTime();
+        }
+        return a.metadata.title.localeCompare(b.metadata.title);
+    });
+
+    let searchTerm = $state('');
+
+    function filterPosts(posts: Post[], search: string): Post[] {
+        const lowerSearch = search.toLowerCase();
+        return posts.filter(post =>
+            post.metadata.title.toLowerCase().includes(lowerSearch) ||
+            post.metadata.summary.toLowerCase().includes(lowerSearch) ||
+            post.metadata.tags.some(tag => tag.toLowerCase().includes(lowerSearch))
+        );
+    }
+
+
+    const filteredPosts = $derived(filterPosts(sortedPosts, searchTerm));
 </script>
 
 <div class="flex flex-col md:flex-row h-screen">
@@ -47,9 +60,9 @@ interface PostMetadata {
         <a class="text-center text-blue-500 underline" href="/info">Info</a>
     </div>
     <h1 class="text-center text-4xl font-bold my-4 underline"><a href="/" >Jacob's Website</a></h1>
-    <input class="h-8 border w-full pl-2" type="text" placeholder="Search posts (this does not currently work)"/>
+    <input class="h-8 border w-full pl-2" type="text" placeholder="Search posts" bind:value={searchTerm} />
     <div class="overflow-y-auto">
-        {#each posts as { slug, metadata }}
+        {#each filteredPosts as { slug, metadata }}
             <div class="my-4">
                 <a href={`/${slug}`} class="font-semibold underline">{metadata.title}</a>
                 <br />
@@ -57,7 +70,7 @@ interface PostMetadata {
                 <br />
                 <div class="flex flex-row">
                     {#each metadata.tags as tag}
-                        <span class="text-sm mr-2">{tag}</span>
+                        <span class="text-sm mr-2 text-yellow-600">#{tag}</span>
                     {/each}
                 </div>
             </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,21 +1,21 @@
-<div class="flex flex-col">
+<div class="flex flex-col max-w-4xl">
 <h1 class="m-auto text-4xl font-bold ">About</h1>
 
-<p class="indent-4 md:indent-8 text-lg/6 md:text-xl/8 my-4">
+<p class="indent-4 md:indent-8 text-lg/6 md:text-xl/8 my-8">
 Hello!! 
 
 I'm Jacob (Wubbzee on some platforms, JGM on others). I am a big fan of computers, and everything involving them.
 
 This website is intended to be a bucket for things I do, as well as opinions and predictions I want to look back on in 5+ years and verify my correctness. This is a fairly broad description, so expect stuff like:
 </p>
-<ul class="text-lg/6 md:text-xl/8 list-disc list-inside">
+<ul class="text-lg/6 md:text-xl/8 list-disc list-inside my-8">
     <li>Art</li>
     <li>Computer talk</li>
     <li>Photography</li>
     <li>Projects</li>
     <li>Jotted-down ideas</li>
 </ul>
-<p class="indent-4 md:indent-8 text-lg/6 md:text-xl/8 my-4">
+<p class="indent-4 md:indent-8 text-lg/6 md:text-xl/8 my-8">
 Improving my writing skills is another goal of this website, I feel like my writing is very boring/plain and would like to see it become more like some folks I've read on sites like lesswrong (lol). 
 </p>
     </div>
